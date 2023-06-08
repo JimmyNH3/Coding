a47842719b3f44343ac068a48f26780a72986e78
NHan3 <jimmy20060504@gmail.com>
2023-06-08 20:24:18 +0800

    update

--- ZeroJudge/g277.cpp
+++ ZeroJudge/g277.cpp
@@ -1,12 +1,49 @@
 #include <bits/stdc++.h>
 #define ouo ios_base::sync_with_stdio(false), cin.tie(0)
 #define ll long long
+#define pii pair<int, int>
 using namespace std;
 
+int arr[100005] = {-1};
+ll sum[100005] = {-1};
+vector <pii> in; 
+
+bool cmp(pii a, pii b) {
+    return a.first < b.first; 
+}
+
 int main() {
+    ouo;
+
     int n;
     cin >> n;
-    int sum[n];
-    for(int i = 0; i < n; i++) cin >> sum[i];
+
+    for(int i = 1; i <= n; i++) cin >> arr[i];
+    sum[1] = arr[1];
+    for(int i = 2; i <= n; i++) sum[i] = arr[i] + sum[i - 1];
+    //for(int i = 1; i <= n; i++) cout << sum[i] << " ";
     
+    //index
+    for(int i = 1; i <= n; i++) {
+        in.push_back({arr[i], i});
+    }
+
+    sort(in.begin(), in.end(), cmp);
+
+    int l = 1, r = n; 
+    int index = l;
+    for(int i = l + 1; i <= r; i++) {
+        if(arr[i] < arr[index]) {
+            index = i;
+        }
+    }
+     
+    while(l < r) {
+        
+
+        if(sum[index - 1] > sum[r] - sum[index]) r = index - 1;
+        else l = index + 1;
+    }
+
+    cout << arr[l];
 }
--- ZeroJudge/tempCodeRunnerFile.cpp
+++ ZeroJudge/tempCodeRunnerFile.cpp
@@ -1,26 +1,31 @@
 #include <bits/stdc++.h>
+#define ouo ios_base::sync_with_stdio(false), cin.tie(0)
+#define ll long long
 using namespace std;
-queue<int> qq;
 
 int main() {
-    ios_base::sync_with_stdio(false), cin.tie(0);
     int n;
     cin >> n;
+    int arr[n], sum[n];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    sum[0] = arr[0];
+    for(int i = 1; i < n; i++) sum[i] = arr[i] + sum[i - 1];
+    for(int i = 0; i < n; i++) cout << sum[i] << " ";
+    int l = 0, r = n - 1, ans;
+    
+    while(l < r) {
+        int minnum = arr[0], index;
 
-    while(n--) {
-        int k, add;
-        cin >> k;
-        
-        switch(k) {
-            case 1:
-                cin >> add;
-                qq.push(add);
-                break;
-            case 2:
-                qq.empty() == true ? cout << qq.front() << endl : cout << -1 << endl;
-                break;
-            case 3:
-                if(qq.empty() == false) qq.pop();
+        for(int i = l; i <= r; i++) {
+            if(arr[i] < minnum) {
+                minnum = arr[i];
+                index = i;
+            }
         }
+
+        if(sum[index - 1] > sum[r] - sum[index]) r = index - 1;
+        else l = index + 1;
     }
+
+    cout << endl << arr[l];
 }
